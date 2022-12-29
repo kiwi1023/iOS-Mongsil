04a12701a27df9952c5db82d7679abcf83e28615
Kiwi <toskw@naver.com>
2022-12-29 14:16:55 +0900

    add: 프로젝트 파일 생성

---
+++ iOS-Mongsil/iOS-Mongsil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Mongsil
+//
+//  Created by Kiwon Song on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
