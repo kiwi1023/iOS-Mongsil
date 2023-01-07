da377b4187bedb2e58ad96f3e02e5b27e36361f3
starting1231 <starting1231@naver.com>
2023-01-07 16:57:39 +0900

    refactor: ViewModelBuilder 수정

--- iOS-Mongsil/iOS-Mongsil/Presentation/BaseProtocol/ViewModelBuilderProtocol.swift
+++ iOS-Mongsil/iOS-Mongsil/Presentation/BaseProtocol/ViewModelBuilderProtocol.swift
@@ -5,9 +5,11 @@
 //  Created by Kiwon Song on 2023/01/06.
 //
 
+import Combine
+
 protocol ViewModelBuilder {
     associatedtype Input
     associatedtype Output
     
-    func transform(input: Input) -> Output
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never>
 }
