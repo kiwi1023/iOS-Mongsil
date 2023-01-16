cc4914ca3ff831a383bf814ad3122d9f8a241ade
starting1231 <starting1231@naver.com>
2023-01-16 19:34:11 +0900

    feat: MenuView 구현

---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/MenuScene/MenuView.swift
@@ -0,0 +1,187 @@
+//
+//  MenuView.swift
+//  iOS-Mongsil
+//
+//  Created by Groot on 2023/01/13.
+//
+
+import UIKit
+
+protocol MenuViewDelegate: NSObject {
+    func didTapShareButton()
+    func didTapScrapImageButton()
+    func didTapDownloadImageButton()
+    func didTapIsShowCommentsButton()
+}
+
+final class MenuView: SuperViewSetting {
+    private var isHiddenComments = false
+    private var isScrappedBackground = false
+    weak var delegate: MenuViewDelegate?
+    
+    private let isShowCommentsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "icViewOff"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    private let isShowCommentsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "일기 숨기기"
+        
+        return label
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "icShare"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    private let shareLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "공유"
+        
+        return label
+    }()
+    
+    private let scrapImageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "icLike"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+
+        button.tintColor = .black
+        
+        return button
+    }()
+    
+    private let scrapImageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "스크랩"
+        
+        return label
+    }()
+    
+    private let downloadImageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "icDownload"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    private let downloadImageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "저장"
+        
+        return label
+    }()
+    
+    func setupIsHiddenComments(_ isHiddenComments: Bool) {
+        self.isHiddenComments = isHiddenComments
+        setIsShowCommentsItems()
+    }
+    
+    func setupIsScrappedBackground(_ isScrappedBackground: Bool) {
+        self.isScrappedBackground = isScrappedBackground
+    }
+    
+    override func setupDefault() {
+        isShowCommentsButton.addTarget(self, action: #selector(didTapIsShowCommentsButton), for: .touchDown)
+        shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchDown)
+        scrapImageButton.addTarget(self, action: #selector(didTapScrapImageButton), for: .touchDown)
+        downloadImageButton.addTarget(self, action: #selector(didTapDownloadImageButton), for: .touchDown)
+    }
+    
+    @objc
+    private func didTapIsShowCommentsButton() {
+        delegate?.didTapIsShowCommentsButton()
+        setIsShowCommentsItems()
+    }
+    
+    private func setIsShowCommentsItems() {
+        isHiddenComments.toggle()
+        isHiddenComments ? isShowCommentsButton.setImage(UIImage(named: "icViewOn"), for: .normal) : isShowCommentsButton.setImage(UIImage(named: "icViewOff"), for: .normal)
+        isShowCommentsLabel.text = isHiddenComments ? "일기 보이기" : "일기 숨기기"
+    }
+    
+    @objc
+    private func didTapShareButton() {
+        delegate?.didTapShareButton()
+    }
+    
+    @objc
+    private func didTapScrapImageButton() {
+        delegate?.didTapScrapImageButton()
+        setIsScrappedCommentItems()
+    }
+    
+    func setIsScrappedCommentItems() {
+        isScrappedBackground ? scrapImageButton.setImage(UIImage(named: "icLikeSel"), for: .normal) : scrapImageButton.setImage(UIImage(named: "icLike"), for: .normal)
+        isScrappedBackground.toggle()
+    }
+    
+    @objc
+    private func didTapDownloadImageButton() {
+        delegate?.didTapDownloadImageButton()
+    }
+    
+    override func addUIComponents() {
+        addSubview(isShowCommentsButton)
+        addSubview(isShowCommentsLabel)
+        addSubview(shareButton)
+        addSubview(shareLabel)
+        addSubview(scrapImageButton)
+        addSubview(scrapImageLabel)
+        addSubview(downloadImageButton)
+        addSubview(downloadImageLabel)
+    }
+    
+    override func setupLayout() {
+        NSLayoutConstraint.activate(
+            [
+                isShowCommentsButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 45),
+                isShowCommentsButton.bottomAnchor.constraint(equalTo: isShowCommentsLabel.topAnchor),
+                isShowCommentsButton.heightAnchor.constraint(equalToConstant: 30),
+                isShowCommentsLabel.centerXAnchor.constraint(equalTo: isShowCommentsButton.centerXAnchor),
+                isShowCommentsLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+            ])
+        NSLayoutConstraint.activate(
+            [
+                shareButton.trailingAnchor.constraint(equalTo: scrapImageButton.leadingAnchor, constant: -25),
+                shareButton.bottomAnchor.constraint(equalTo: shareLabel.topAnchor),
+                shareButton.heightAnchor.constraint(equalToConstant: 30),
+                shareLabel.centerXAnchor.constraint(equalTo: shareButton.centerXAnchor),
+                shareLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+                
+            ])
+        NSLayoutConstraint.activate(
+            [
+                scrapImageButton.trailingAnchor.constraint(equalTo: downloadImageButton.leadingAnchor, constant: -25),
+                scrapImageButton.bottomAnchor.constraint(equalTo: scrapImageLabel.topAnchor),
+                scrapImageButton.heightAnchor.constraint(equalToConstant: 30),
+                scrapImageLabel.centerXAnchor.constraint(equalTo: scrapImageButton.centerXAnchor),
+                scrapImageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+            ])
+        NSLayoutConstraint.activate(
+            [
+                downloadImageButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25),
+                downloadImageButton.bottomAnchor.constraint(equalTo: downloadImageLabel.topAnchor),
+                downloadImageButton.heightAnchor.constraint(equalToConstant: 30),
+                downloadImageLabel.centerXAnchor.constraint(equalTo: downloadImageButton.centerXAnchor),
+                downloadImageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+            ])
+    }
+}
---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/MenuScene/MenuViewController.swift
@@ -0,0 +1,155 @@
+//
+//  MenuViewController.swift
+//  iOS-Mongsil
+//
+//  Created by Groot on 2023/01/13.
+//
+
+import UIKit
+import Combine
+
+
+protocol MenuViewControllerDelegate: AnyObject {
+    func didTapIsShowCommentsButton()
+}
+
+final class MenuViewController: SuperViewControllerSetting {
+    private let menuView = MenuView()
+    private let viewModel: MenuViewModel
+    private let input = PassthroughSubject<MenuViewModel.Input, Never>()
+    private var cancellables = Set<AnyCancellable>()
+    private let isHiddenComments: Bool
+    private var backgroundImage: UIImage?
+    weak var delegate: MenuViewControllerDelegate?
+    
+    init(date: Date, image: BackgroundImage, isHiddenComments: Bool) {
+        self.viewModel = MenuViewModel(date: date, image: image)
+        self.isHiddenComments = isHiddenComments
+        super.init()
+    }
+    
+    required init() {
+        fatalError("init() has not been implemented")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        input.send(.viewDidLoad)
+    }
+    
+    override func setupDefault() {
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        bindViewModel()
+        setupTapGesture()
+        menuView.delegate = self
+        menuView.setupIsHiddenComments(isHiddenComments)
+    }
+    
+    private func bindViewModel() {
+        let output = viewModel.transform(input: input.eraseToAnyPublisher())
+        output.sink { [weak self] event in
+            guard let self = self else { return }
+            
+            switch event {
+            case .postBackgroundURL(let url):
+                self.fetchBackgroundImage(url: url)
+            case .postIsScrappedBackgorund(let result):
+                self.setupScrapButton(isScrappedBackground: result)
+            case .dataBaseFailure(_):
+                break
+            }
+        }.store(in: &cancellables)
+    }
+    
+    private func fetchBackgroundImage(url: URL) {
+        ImageCacheManager.shared.load(url: url)
+            .sink { completion in
+                switch completion {
+                case .failure(_):
+                    break
+                case .finished:
+                    break
+                }
+            } receiveValue: { [weak self] image in
+                guard let self = self else { return }
+                
+                let size = CGSize(width: self.view.frame.width, height: self.view.frame.height)
+                self.backgroundImage = image?.resizeImageTo(size: size)
+            }.store(in: &cancellables)
+    }
+    
+    private func setupScrapButton(isScrappedBackground: Bool) {
+        menuView.setupIsScrappedBackground(isScrappedBackground)
+        menuView.setIsScrappedCommentItems()
+    }
+    
+    private func setupTapGesture() {
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    override func addUIComponents() {
+        view.addSubview(menuView)
+    }
+    
+    override func setupLayout() {
+        NSLayoutConstraint.activate(
+            [
+                menuView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                menuView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                menuView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                menuView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25)
+            ])
+    }
+}
+
+extension MenuViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        guard touch.view?.isDescendant(of: menuView) == false else { return false }
+        
+        dismiss(animated: true)
+        return true
+    }
+}
+
+extension MenuViewController: MenuViewDelegate {
+    func didTapIsShowCommentsButton() {
+        delegate?.didTapIsShowCommentsButton()
+    }
+    
+    func didTapShareButton() {
+        let activityViewController = UIActivityViewController(activityItems: [backgroundImage as Any],
+                                                              applicationActivities: nil)
+        activityViewController.title = "공유"
+        activityViewController.modalPresentationStyle = .fullScreen
+        present(activityViewController, animated: true)
+    }
+    
+    func didTapScrapImageButton() {
+        input.send(.didTapScrappedButton)
+    }
+    
+    func didTapDownloadImageButton() {
+        downloadBackgroundImage()
+    }
+    
+    private func downloadBackgroundImage() {
+        guard let image = backgroundImage else { return }
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(alertSavedImage), nil)
+    }
+    
+    @objc
+    private func alertSavedImage(image: UIImage, didFinishSavingWithError error: Error, contextInfo: UnsafeMutableRawPointer?) {
+        let alertController = UIAlertController(title: "완료", message: "이미지가 갤러리에 저장되었습니다.", preferredStyle: .alert)
+        alertController.addAction(.init(title: "확인", style: .cancel))
+        present(alertController, animated: true)
+    }
+}
+
+
