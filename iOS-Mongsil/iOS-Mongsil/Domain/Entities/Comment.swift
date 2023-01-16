1a7085e905292e89b58f7a451a5843f3c9375229
starting1231 <starting1231@naver.com>
2023-01-16 20:08:26 +0900

    feat: Comment 타입 수정 및 Color 추가

--- iOS-Mongsil/iOS-Mongsil/Domain/Entities/Comment.swift
+++ iOS-Mongsil/iOS-Mongsil/Domain/Entities/Comment.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import UIKit.UIColor
+import UIKit.UIImage
 
 struct Comment {
     let id: UUID
@@ -14,7 +16,7 @@ struct Comment {
     var text: String
 }
 
-enum Emoticon: String {
+enum Emoticon: String, CaseIterable{
     case happy
     case pleasure
     case satisfied
@@ -33,5 +35,52 @@ enum Emoticon: String {
     
     var name: String {
         return self.rawValue
+    }
+    
+    var image: UIImage? {
+        return UIImage(named: rawValue)
+    }
+    
+    var label: String {
+        switch self {
+        case .happy:
+            return "행복"
+        case .pleasure:
+            return "기쁨"
+        case .satisfied:
+            return "만족"
+        case .notBad:
+            return "보통"
+        case .tired:
+            return "피곤"
+        case .embarrassed:
+            return "창피"
+        case .bored:
+            return "지루함"
+        case .angry:
+            return "화남"
+        case .disgusting:
+            return "불쾌"
+        case .disappointed:
+            return "실망"
+        case .anxious:
+            return "불안"
+        case .depressed:
+            return "우울"
+        case .sad:
+            return "슬픔"
+        case .surprised:
+            return "놀람"
+        case .lonely:
+            return "외로움"
+        }
+    }
+    
+    var labelColor: UIColor? {
+        return UIColor(named: self.rawValue + "LabelColor")
+    }
+    
+    var backgroundColor: UIColor? {
+        return UIColor(named: self.rawValue + "BackgroundColor")
     }
 }
