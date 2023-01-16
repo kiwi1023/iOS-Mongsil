1960ff6de452310c0c202d5d68dfe53284d56070
Kiwi <toskw@naver.com>
2023-01-16 19:58:25 +0900

    feat: FavoriteViewModel 생성

---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/FavoriteView/ViewModel/FavoriteViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  FavoriteViewModel.swift
+//  iOS-Mongsil
+//
+//  Created by Kiwon Song on 2023/01/16.
+//
+
+import Foundation
+import Combine
+
+final class FavoriteViewModel: ViewModelBuilder {
+    typealias Input = FavoriteViewModelInput
+    typealias Output = FavoriteViewModelOutput
+    
+    private var cancellable = Set<AnyCancellable>()
+    private let diaryUseCase = DefaultDiaryUseCase(
+        repositoryManager: DefaultDiaryRepositoryManager(
+            repository: CoreDataDiaryRepository()))
+    private let output = PassthroughSubject<Output, Never>()
+   
+    enum FavoriteViewModelInput {
+        case viewDidLoad
+        case didTapCell(Diary)
+    }
+    
+    enum FavoriteViewModelOutput {
+        case fetchFavoriteDataSuccess([Diary])
+        case fetchFavoriteDataFailure(Error)
+        case fetchCellDiaryData(Diary)
+    }
+    
+    func transform(input: AnyPublisher<FavoriteViewModelInput, Never>) -> AnyPublisher<FavoriteViewModelOutput, Never> {
+        input.sink { [weak self] event in
+            guard let self = self else { return }
+            
+            switch event {
+            case .viewDidLoad:
+                self.fetchFavoriteData()
+            case .didTapCell(let diary):
+                self.output.send(.fetchCellDiaryData(diary))
+            }
+        }.store(in: &cancellable)
+        
+        return output.eraseToAnyPublisher()
+    }
+    
+    private func fetchFavoriteData() {
+        diaryUseCase.read()
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else {return }
+                switch completion {
+                case .failure(let error):
+                    self.output.send(.fetchFavoriteDataFailure(error))
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] data in
+                guard let self = self else { return }
+                
+                self.output.send(.fetchFavoriteDataSuccess(data))
+            }).store(in: &cancellable)
+    }
+}
+
