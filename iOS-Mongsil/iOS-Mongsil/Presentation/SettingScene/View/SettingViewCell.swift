80e2714de620a6ef65857df53eb8df2df4250a7f
Kiwi <toskw@naver.com>
2023-01-17 16:50:49 +0900

    feat: SettingViewCell 생성

---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/SettingScene/View/SettingViewCell.swift
@@ -0,0 +1,85 @@
+//
+//  SettingViewCell.swift
+//  iOS-Mongsil
+//
+//  Created by Kiwon Song on 2023/01/17.
+//
+
+import UIKit
+
+final class SettingViewCell: UICollectionViewCell {
+  
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addUIComponents()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: NSCoder())
+        debugPrint("ProductListViewController Initialize error")
+    }
+    
+    private func addUIComponents() {
+        addSubview(titleImageView)
+        addSubview(titleLabel)
+        addSubview(chevronImage)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            titleImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            titleImageView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            titleImageView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+            titleImageView.widthAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: titleImageView.trailingAnchor, constant: 8),
+            titleLabel.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        NSLayoutConstraint.activate([
+            chevronImage.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            chevronImage.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            chevronImage.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -15),
+            chevronImage.widthAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont(name: "GamjaFlower-Regular", size: 23)
+        label.translatesAutoresizingMaskIntoConstraints=false
+        
+        return label
+    }()
+    
+    private let titleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints=false
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private let chevronImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints=false
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    func configure(text: String, image: String, needChevronImage: Bool) {
+     
+    }
+    
+    override func prepareForReuse() {
+     
+    }
+}
---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/SettingScene/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  iOS-Mongsil
+//
+//  Created by Kiwon Song on 2023/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    let settingViewCell = SettingViewCell()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        view.addSubview(settingViewCell)
+//        settingViewCell.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            settingViewCell.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            settingViewCell.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//            settingViewCell.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            settingViewCell.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+//        ])
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
