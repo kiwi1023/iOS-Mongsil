2b1dbec577a7f2af7f00aacc77c18e3a8c6546e8
Kiwi <toskw@naver.com>
2023-01-17 18:41:52 +0900

    feat: navigationBar 투명 설정

--- iOS-Mongsil/iOS-Mongsil/Application/AppDelegate.swift
+++ iOS-Mongsil/iOS-Mongsil/Application/AppDelegate.swift
@@ -10,6 +10,10 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().isTranslucent = true
         return true
     }
 
