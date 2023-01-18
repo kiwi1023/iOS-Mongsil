cb818d028633e3b00c5f47d86bbf4e5d526ae2cc
starting1231 <starting1231@naver.com>
2023-01-18 18:51:20 +0900

    feat: NotificationView 구현

---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/NotificationScene/NotificationViewController.swift
@@ -0,0 +1,100 @@
+//
+//  NotificationViewController.swift
+//  iOS-Mongsil
+//
+//  Created by Groot on 2023/01/17.
+//
+
+import UIKit
+import UserNotifications
+
+final class NotificationViewController: SuperViewControllerSetting {
+    private let notificationView = NotificationView()
+    private let notificationCenter = UNUserNotificationCenter.current()
+    private let userDefault = UserDefaults.standard
+    private var selectedDateComponents: DateComponents?
+    private var isOnNotification: Bool {
+        get {
+            userDefault.object(forKey: "isOnNotification") as? Bool ?? false
+        }
+        
+        set {
+            userDefault.set(newValue, forKey: "isOnNotification")
+        }
+    }
+    
+    override func setupDefault() {
+        let attributes = [
+            NSAttributedString.Key.font: UIFont(name: "GamjaFlower-Regular", size: 23) ?? UIFont(),
+            NSAttributedString.Key.foregroundColor: UIColor(named: "weekdayColor") as Any
+        ]
+        navigationController?.navigationBar.titleTextAttributes = attributes
+        navigationItem.title = "알림 설정"
+        notificationView.setupIsOnNotification(isOnNotification)
+        notificationView.delegate = self
+    }
+    
+    override func addUIComponents() {
+        view?.addSubview(notificationView)
+    }
+    
+    override func setupLayout() {
+        NSLayoutConstraint.activate([
+            notificationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            notificationView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            notificationView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            notificationView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+}
+
+extension NotificationViewController: NotificationViewDelegate {
+    func didTapToggleButton() {
+        requestNotificationAuthorization()
+        if isOnNotification == true {
+            isOnNotification = false
+            notificationCenter.removeAllPendingNotificationRequests()
+        } else if isOnNotification == false {
+            isOnNotification = true
+            registerNotifocation()
+        }
+        
+        notificationView.setupIsOnNotification(isOnNotification)
+    }
+    
+    private func requestNotificationAuthorization() {
+        let authOptions: UNAuthorizationOptions = [.alert, .sound, .badge]
+        
+        notificationCenter.requestAuthorization(options: authOptions) { success, error in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+    
+    private func registerNotifocation() {
+        guard let dateComponents = selectedDateComponents, isOnNotification == true else { return }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "몽실"
+        content.body = "오늘의 기분을 입력하세요."
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: "Mosil", content: content, trigger: trigger)
+        notificationCenter.removeAllPendingNotificationRequests()
+        notificationCenter.add(request)
+    }
+    
+    func didTapChevronButton() {
+        let viewController = DatePickerViewContoller()
+        viewController.delegate = self
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: true)
+    }
+}
+
+extension NotificationViewController: DatePickerViewContollerDelegate {
+    func didTapConfirmButton(dateComponents: DateComponents) {
+        selectedDateComponents = dateComponents
+        registerNotifocation()
+    }
+}
---
+++ iOS-Mongsil/iOS-Mongsil/Presentation/NotificationScene/View/NotificationView.swift
@@ -0,0 +1,105 @@
+//
+//  NotificationView.swift
+//  iOS-Mongsil
+//
+//  Created by Groot on 2023/01/17.
+//
+
+import UIKit
+
+protocol NotificationViewDelegate: AnyObject {
+    func didTapToggleButton()
+    func didTapChevronButton()
+}
+
+final class NotificationView: SuperViewSetting {
+    private var isOnNotification = false
+    private var buttonImage: UIImage? {
+        isOnNotification ? UIImage(named: "icSwitchOn") : UIImage(named: "icSwitchOff")
+    }
+    weak var delegate: NotificationViewDelegate?
+    
+    private let alarmLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "GamjaFlower-Regular", size: 23)
+        label.text = "알림"
+        
+        return label
+    }()
+    
+    private let toggleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(nil, action: #selector(didTapToggleButton), for: .touchDown)
+        
+        return button
+    }()
+    
+    private let selectTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "GamjaFlower-Regular", size: 23)
+        label.text = "시간 선택"
+        
+        return label
+    }()
+    
+    private let chevronButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(nil, action: #selector(didTapChevronButton), for: .touchDown)
+        button.setImage(UIImage(named: "icCloseCopy"), for: .normal)
+        
+        return button
+    }()
+    
+    func setupIsOnNotification(_ isOnNotification: Bool) {
+        self.isOnNotification = isOnNotification
+        setupIsHiddenView()
+    }
+    
+    @objc
+    private func didTapToggleButton() {
+        delegate?.didTapToggleButton()
+    }
+    
+    @objc
+    private func didTapChevronButton() {
+        delegate?.didTapChevronButton()
+    }
+    
+    private func setupIsHiddenView() {
+        selectTimeLabel.isHidden = !isOnNotification
+        chevronButton.isHidden = !isOnNotification
+        toggleButton.setImage(buttonImage, for: .normal)
+    }
+    
+    override func addUIComponents() {
+        addSubview(alarmLabel)
+        addSubview(toggleButton)
+        addSubview(selectTimeLabel)
+        addSubview(chevronButton)
+    }
+    
+    override func setupLayout() {
+        NSLayoutConstraint.activate([
+            alarmLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            alarmLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            toggleButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            toggleButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            selectTimeLabel.topAnchor.constraint(equalTo: alarmLabel.bottomAnchor, constant: 30),
+            selectTimeLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            chevronButton.topAnchor.constraint(equalTo: toggleButton.bottomAnchor, constant: 30),
+            chevronButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
