474d3f9c1caeab2a48c1cd41ac039b7b9279bcc9
Kiwi <toskw@naver.com>
2023-01-19 21:09:03 +0900

    feat: luanchScreen 추가

--- iOS-Mongsil/iOS-Mongsil/Application/AppDelegate.swift
+++ iOS-Mongsil/iOS-Mongsil/Application/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(1)
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
         UINavigationBar.appearance().shadowImage = UIImage()
         UINavigationBar.appearance().backgroundColor = .clear
