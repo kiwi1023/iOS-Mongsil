681950f5d2e7008b47e2c5974b60cdd4fb2b326f
starting1231 <starting1231@naver.com>
2023-01-30 21:32:37 +0900

    feat: MultipleRepositoryManager 구현

---
+++ iOS-Mongsil/iOS-Mongsil/Data/MultipleRepositoryManager/MultipleCommentRepositoryManager.swift
@@ -0,0 +1,41 @@
+//
+//  MultipleCommentRepositoryManager.swift
+//  iOS-Mongsil
+//
+//  Created by Groot on 2023/01/30.
+//
+
+import Foundation
+import Combine
+
+final class MultipleCommentRepositoryManager: CommentRepositoryManager {
+    var repository: CommentRepository
+    private var remoteRepository: CommentRepository
+    
+    init(repository: CommentRepository = CoreDataCommentRepository(), remoteRepository: CommentRepository = FirebaseCommentRepository()) {
+        self.repository = repository
+        self.remoteRepository = remoteRepository
+    }
+    
+    func create(input: Comment) -> AnyPublisher<Void, Error> {
+        let _ = remoteRepository.create(input: input)
+        
+        return repository.create(input: input)
+    }
+    
+    func read() -> AnyPublisher<[Comment], Error> {
+        repository.read()
+    }
+    
+    func update(input: Comment) -> AnyPublisher<Void, Error> {
+        let _ = remoteRepository.update(input: input)
+        
+        return repository.update(input: input)
+    }
+    
+    func delete(id: UUID) -> AnyPublisher<Void, Error> {
+        let _ = remoteRepository.delete(id: id)
+        
+        return repository.delete(id: id)
+    }
+}
---
+++ iOS-Mongsil/iOS-Mongsil/Data/MultipleRepositoryManager/MultipleDiaryRepositoryManager.swift
@@ -0,0 +1,35 @@
+//
+//  MultipleDiaryRepositoryManager.swift
+//  iOS-Mongsil
+//
+//  Created by Groot on 2023/01/30.
+//
+
+import Combine
+
+final class MultipleDiaryRepositoryManager: DiaryRepositoryManager {
+    var repository: DiaryRepository
+    private var remoteRepository: DiaryRepository
+    
+    init(repository: DiaryRepository = CoreDataDiaryRepository(), remoteRepository: DiaryRepository = FirebaseDiaryRepository()) {
+        self.repository = repository
+        self.remoteRepository = remoteRepository
+    }
+    
+    func create(input: Diary) -> AnyPublisher<Void, Error> {
+        let _ = remoteRepository.create(input: input)
+        
+        return repository.create(input: input)
+    }
+    
+    func read() -> AnyPublisher<[Diary], Error> {
+        repository.read()
+    }
+    
+    func delete(id: String) -> AnyPublisher<Void, Error> {
+        let _ = remoteRepository.delete(id: id)
+        
+        return repository.delete(id: id)
+    }
+}
+
